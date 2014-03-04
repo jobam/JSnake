5df8adaeb49f74e752ced4df6daf608b34d32d6e
abraha_j <abraha_j@epitech.eu>
2014-03-04 13:39:23 +0100

    et la police alors

---
+++ src/snake/Main.java
@@ -0,0 +1,13 @@
+package snake;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Game	game = new Game();
+
+		  game.exec();
+		  System.out.println("Made by Abraham Jonathan in Java with JSFML 2.0");
+
+	}
+
+}
